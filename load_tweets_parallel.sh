deedfc5eb5c1d616dce30bc4269e6be154273e19
echen4628 <echen4628@gmail.com>
2024-04-11 01:00:54 -0700

    working on deduping

--- load_tweets_parallel.sh
+++ load_tweets_parallel.sh
@@ -13,9 +13,9 @@ time echo "$files" | parallel ./load_denormalized.sh
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
-# FIXME: implement this with GNU parallel
+time echo "$files" | parallel ./load_normalized.sh
 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
-# FIXME: implement this with GNU parallel
+time echo "$files" | parallel ./load_tweets_batch.sh
